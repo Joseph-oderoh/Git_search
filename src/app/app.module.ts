a9dce09abe8b2f4ef9dec596c08f2a37ddb0a558
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-04-16 19:45:41 +0300

    style: create mew user and repositoriy component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepositoryComponent
+    RepositoryComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
